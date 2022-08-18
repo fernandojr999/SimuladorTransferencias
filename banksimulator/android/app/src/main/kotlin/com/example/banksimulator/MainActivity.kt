6827114937fbdae60d6174c9ddf5bf1d1e8ad219
Fernando Junior <fernando.junior@sankhya.com.br>
2022-08-18 07:20:50 -0300

    Carga do projeto inicial

---
+++ banksimulator/android/app/src/main/kotlin/com/example/banksimulator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.banksimulator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ banksimulator/lib/main.dart
@@ -0,0 +1,56 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "Transferências",
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.add),
+          ),
+          body: ListaTransferencias()),
+    ));
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(100.0, 1000)),
+        ItemTransferencia(Transferencia(200.0, 2000)),
+        ItemTransferencia(Transferencia(300.0, 3000)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
